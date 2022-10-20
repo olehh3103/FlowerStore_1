63446d441b6feb391fbb9f26786561b84dc9f0fc
Asus <olehh3103@gmail.com>
2022-10-20 22:46:54 +0300

    total5 final

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Assertions;
 
 public class StoreTest {
     private Store store;
-//    static final int MAX_SCORE = 100;
-//    static final int MAX_SCORE_TWO = 123;
 
     @BeforeEach
     public void init() {
@@ -20,16 +18,10 @@ public class StoreTest {
         Flower flowF = new Flower();
         FlowerColor colorF = FlowerColor.RED;
         flowF.setColor(colorF);
-        int MAX_RANDOM_NUMBER = 100;
-        int MAX_RANDOM_NUMBER_SECOND = 2324;
-        flowF.setPrice(MAX_RANDOM_NUMBER);
-        flowF.setSepalLength(MAX_RANDOM_NUMBER_SECOND);
 
         Flower flowS = new Flower();
         FlowerColor colorS = FlowerColor.BLUE;
         flowS.setColor(colorS);
-        flowS.setPrice(MAX_RANDOM_NUMBER);
-        flowS.setSepalLength(MAX_RANDOM_NUMBER_SECOND);
 
         store.addFlow(flowF);
         Assertions.assertNull(store.search(flowS));
