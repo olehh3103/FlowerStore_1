4f19411b0cf96fff0bb036569ec6dc5236095f34
Asus <olehh3103@gmail.com>
2022-10-20 20:30:51 +0300

    fghj

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import java.util.Random;
 
 public class FlowerBucketTest {
-//    private static final Random RANDOM_GENERATOR = new Random();
-//    private static final int MAX_QUANTITY = 1000;
-//    private static final int MAX_PRICE = 100;
+    private static final Random RANDOM_GENERATOR = new Random();
+    private static final int MAX_QUANTITY = 1000;
+    private static final int MAX_PRICE = 100;
 
     private FlowerBucket flowerBucket;
 
@@ -20,8 +20,8 @@ public class FlowerBucketTest {
 
     @Test
     public void testPrice() {
-        int price = 123;
-        int quantity = 2;
+        int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
+        int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
         Flower flower = new Rose();
         flower.setPrice(price);
         FlowerPack flowerPack = new FlowerPack(flower, quantity);
